ba3fbf4d30f68a5ca930ccd3cdc16343c0162ccd
samplemckee <86639242+YuTheGreat-ck@users.noreply.github.com>
2026-05-27 17:09:32 +0800

    Update manifest.ts

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -1,10 +1,11 @@
+// src/app/manifest.ts
 import { MetadataRoute } from "next";
 
 export default function manifest(): MetadataRoute.Manifest {
   return {
     name: "UIXSHUVO Industrial",
     short_name: "UIXSHUVO",
-    description: "高性能工业吸油绵，全球工业环保泄漏应急处理专家",
+    description: "高性能工业吸油绵，工业环保泄漏应急专家",
     start_url: "/",
     display: "standalone",
     background_color: "#ffffff",
