7f18e341d117070af4bc3ed47fbf5a0a4654d454
samplemckee <86639242+YuTheGreat-ck@users.noreply.github.com>
2026-05-27 12:51:51 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: 'export',      // 强制 Next.js 输出为纯静态 HTML
+  images: {
+    unoptimized: true,   // 静态导出时必须禁用内置图片优化
+  },
+}
 
 module.exports = nextConfig
