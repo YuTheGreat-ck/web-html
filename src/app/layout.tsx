8859de264d5a845109c681e6e4046c640f371178
samplemckee <86639242+YuTheGreat-ck@users.noreply.github.com>
2026-05-27 15:59:13 +0800

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,41 +1,14 @@
-import { Ubuntu } from "next/font/google";
-import type { Metadata } from "next";
-import "./globals.css";
-
-import { METADATA } from "@/common/constant/metadata";
-import Layout from "@/components/layouts";
-
-const ubuntu = Ubuntu({
-  weight: ["400", "500", "700"],
-  subsets: ["latin"],
-});
-
-const siteUrl =
-  process.env.NODE_ENV === "development"
-    ? "http://localhost:3000"
-    : process.env.DOMAIN ||
-      process.env.NEXT_PUBLIC_SITE_URL ||
-      "https://example.com";
-
 export const metadata: Metadata = {
   metadataBase: new URL(siteUrl),
 
   title: {
-    default: "Real Estate",
-    template: "%s | Real Estate",
+    default: "UIXSHUVO | 工业吸油绵",
+    template: "%s | UIXSHUVO",
   },
 
   description: METADATA.description,
-
   keywords: METADATA.keyword,
-
-  authors: [
-    {
-      name: METADATA.authors.name,
-      url: METADATA.authors.url,
-    },
-  ],
-
+  authors: [{ name: METADATA.authors.name, url: METADATA.authors.url }],
   creator: METADATA.creator,
 
   openGraph: {
@@ -45,15 +18,12 @@ export const metadata: Metadata = {
     locale: METADATA.openGraph.locale,
     siteName: METADATA.openGraph.siteName,
     description: METADATA.openGraph.description,
-
-    images: [
-      {
-        url: METADATA.openGraph.images.url,
-        width: 1200,
-        height: 630,
-        alt: METADATA.openGraph.images.alt,
-      },
-    ],
+    images: [{
+      url: METADATA.openGraph.images.url,
+      width: 1200,
+      height: 630,
+      alt: METADATA.openGraph.images.alt,
+    }],
   },
 
   twitter: {
@@ -61,25 +31,6 @@ export const metadata: Metadata = {
     title: METADATA.title,
     description: METADATA.description,
     images: [`${siteUrl}/og.png`],
-    creator: "@abdurrozaqf_",
   },
-
   manifest: METADATA.manifest,
 };
-
-interface RootLayoutProps {
-  children: React.ReactNode;
-}
-
-export default function RootLayout({
-  children,
-}: RootLayoutProps) {
-  return (
-    <html lang="en">
-      <head />
-      <body className={ubuntu.className}>
-        <Layout>{children}</Layout>
-      </body>
-    </html>
-  );
-}
