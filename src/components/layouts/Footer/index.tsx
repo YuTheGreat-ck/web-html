58b721129b5a27618600074062d7ebcc02c4588e
samplemckee <86639242+YuTheGreat-ck@users.noreply.github.com>
2026-05-27 18:32:03 +0800

    Update index.tsx

--- src/components/layouts/Footer/index.tsx
+++ src/components/layouts/Footer/index.tsx
@@ -1,9 +1,5 @@
 import React from "react";
-import {
-  MENU_ABOUT_US,
-  MENU_CONTACT_INFO,
-  MENU_QUICK,
-} from "@/common/constant/menu";
+import { MENU_ABOUT_US, MENU_CONTACT_INFO, MENU_QUICK } from "@/common/constant/menu";
 
 import ContactInfo from "./ContactInfo";
 import QuickMenu from "./QuickMenu";
@@ -11,19 +7,28 @@ import AboutUs from "./AboutUs";
 import Tagline from "./Tagline";
 
 export default function Footer() {
+  const currentYear = new Date().getFullYear();
+
   return (
-    <footer className="w-full px-4 lg:px-10 xl:px-36 py-4 lg:py-8 bg-black text-white flex flex-col">
-      <div className="flex flex-col lg:flex-row justify-between gap-8">
-        <Tagline />
-        <div className="ml-0 xl:ml-24 2xl:ml-36 w-full flex flex-wrap justify-between items-start gap-y-8">
+    <footer className="w-full px-6 py-12 lg:px-20 xl:px-36 bg-black text-white">
+      <div className="flex flex-col lg:flex-row justify-between gap-12 lg:gap-8">
+        {/* 品牌与订阅部分 */}
+        <div className="lg:w-1/3">
+          <Tagline />
+        </div>
+        
+        {/* 导航菜单区域 */}
+        <div className="flex-1 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 lg:ml-12 xl:ml-24">
           <QuickMenu data={MENU_QUICK} />
           <AboutUs data={MENU_ABOUT_US} />
           <ContactInfo data={MENU_CONTACT_INFO} />
         </div>
       </div>
-      <hr className="my-6" />
-      <div className="text-center text-sm text-neutral-300">
-        @ 2023 UiXSHUVO All Rights Reserved
+
+      <hr className="my-10 border-neutral-800" />
+      
+      <div className="text-center text-sm text-neutral-400">
+        © {currentYear} UIXSHUVO. All Rights Reserved.
       </div>
     </footer>
   );
